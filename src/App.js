fd255ac2ad2b2237fe4503c3dd516ef11e3074b8
Will Zakielarz <wmz1st@gmail.com>
2024-04-15 20:39:29 -0400

    start of cards

--- src/App.js
+++ src/App.js
@@ -6,17 +6,17 @@ export default function App() {
   return (
     <div>
       {/* Navbar: Logo and title */}
-      <div className="flex items-center justify-between p-2">
+      <div className="flex flex-row items-center p-2">
         <img src={Logo} alt="logo" className="h-12" />
         <h1 className="text-2xl font-bold ml-4">QuikCal Dashboard</h1>
       </div>
       {/* Body */}
-      <div className="flex flex-col items-center p-4 bg-gray-300 h-[calc(100vh-64px)]">
-        <div className="flex items-center justify-center w-3/4 h-[75vh]">
+      <div className="flex flex-col items-center p-4 bg-gray-200 h-[calc(100vh-64px)]">
+        <div className="flex items-center justify-center w-3/4 h-[75vh] bg-white rounded-2xl shadow-lg">
           <BarGraph />
         </div>
 
-        <div className="flex flex-wrap justify-center h-[25vh] overflow-auto">
+        <div className="flex flex-wrap justify-center h-[25vh] overflow-auto bg-white rounded-2xl shadow-lg mt-4 m">
           <CompanyCard />
           <CompanyCard />
           <CompanyCard />
--- src/components/CompanyCard.js
+++ src/components/CompanyCard.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const CompanyCard = () => {
-  return <h1 className="text-2xl font-bold ml-4">Company Name</h1>;
+  // Grey, rounded-edges card with a company name
+  return (
+    <div className="flex items-center justify-start w-full h-1/4 bg-gray-200 m-2 rounded-lg">
+      <h2 className="text-xl font-bold pl-4 text-gray-500">Company Name</h2>
+    </div>
+  );
 };
 
 export default CompanyCard;
