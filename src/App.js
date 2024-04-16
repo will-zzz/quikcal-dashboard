432fe3250e63b1feec507a2caee117a183ff0a1b
Will Zakielarz <wmz1st@gmail.com>
2024-04-15 21:46:29 -0400

    Cards look pretty deece

--- src/App.js
+++ src/App.js
@@ -12,15 +12,34 @@ export default function App() {
       </div>
       {/* Body */}
       <div className="flex flex-col items-center p-4 bg-gray-200 h-[calc(100vh-64px)]">
-        <div className="flex items-center justify-center w-3/4 h-[75vh] bg-white rounded-2xl shadow-lg">
-          <BarGraph />
+        <div className="w-3/4 h-[65vh] bg-white rounded-2xl shadow-lg flex justify-center items-center">
+          {/* Using this weird styling here because idk the graph library is being weird */}
+          <div style={{ height: "95%", width: "95%" }}>
+            <BarGraph />
+          </div>
         </div>
 
-        <div className="flex flex-wrap justify-center h-[25vh] overflow-auto bg-white rounded-2xl shadow-lg mt-4 m">
-          <CompanyCard />
-          <CompanyCard />
-          <CompanyCard />
-          <CompanyCard />
+        <div className="flex flex-wrap justify-center w-3/4 h-[35vh] overflow-auto bg-white rounded-2xl shadow-lg mt-4 pt-1">
+          <CompanyCard
+            name="Company Name"
+            time="12:30 PM - 1:30 PM"
+            info="Info about the delivery."
+          />
+          <CompanyCard
+            name="Company Name"
+            time="12:30 PM - 1:30 PM"
+            info="Info about the delivery."
+          />
+          <CompanyCard
+            name="Company Name"
+            time="12:30 PM - 1:30 PM"
+            info="Info about the delivery."
+          />
+          <CompanyCard
+            name="Company Name"
+            time="12:30 PM - 1:30 PM"
+            info="Info about the delivery."
+          />
         </div>
       </div>
     </div>
--- src/components/CompanyCard.js
+++ src/components/CompanyCard.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
-const CompanyCard = () => {
-  // Grey, rounded-edges card with a company name
+const CompanyCard = ({ name, time, info }) => {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
+
   return (
-    <div className="flex items-center justify-start w-full h-1/4 bg-gray-200 m-2 rounded-lg">
-      <h2 className="text-xl font-bold pl-4 text-gray-500">Company Name</h2>
+    <div className="flex flex-col w-full h-auto bg-gray-200 mx-2 my-1 px-4 rounded-lg">
+      <div className="flex items-center justify-between p-4">
+        <div className="flex items-center">
+          <h2 className="text-xl font-bold text-gray-500">{name}</h2>
+          <p className="pl-4 text-gray-500">{time}</p>
+        </div>
+        <div className="flex items-center">
+          <p className="text-m font-bold pl-4 text-gray-500 mr-2">Completed</p>
+          <button className="w-7 h-7 rounded-full border-2 border-gray-500"></button>
+          <button onClick={toggleDropdown} className="mx-2 text-gray-500">
+            v
+          </button>
+        </div>
+      </div>
+      {dropdownOpen && <p className="text-gray-500 text-m mx-4 mb-2">{info}</p>}
     </div>
   );
 };
