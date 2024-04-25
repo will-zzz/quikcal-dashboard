ef12ab61b0bf21baa067786928530ff7fd55bee9
Will Zakielarz <wmz1st@gmail.com>
2024-04-25 15:22:19 -0400

    Dropdown arrow improvement
    Added clickable svg for card arrow.

--- src/components/CompanyCard.js
+++ src/components/CompanyCard.js
@@ -18,10 +18,34 @@ const CompanyCard = ({ key, name, start, end, info }) => {
         </div>
         <div className="flex items-center">
           <p className="text-m font-bold pl-4 text-gray-500 mr-2">Completed</p>
-          <button className="w-7 h-7 rounded-full border-2 border-gray-500"></button>
-          <button onClick={toggleDropdown} className="mx-2 text-gray-500">
-            v
-          </button>
+          <button
+            className="w-7 h-7 rounded-full border-2 border-gray-500 mr-2"
+            onClick={() => {}}
+          ></button>
+          {/* Dropdown arrow */}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="2"
+            className="w-6 h-6 cursor-pointer stroke-gray-500"
+            onClick={() => toggleDropdown()}
+          >
+            {!dropdownOpen && (
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="m4.5 15.75 7.5-7.5 7.5 7.5"
+              />
+            )}
+            {dropdownOpen && (
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="m19.5 8.25-7.5 7.5-7.5-7.5"
+              />
+            )}
+          </svg>
         </div>
       </div>
       {dropdownOpen && <p className="text-gray-500 text-m mx-4 mb-2">{info}</p>}
