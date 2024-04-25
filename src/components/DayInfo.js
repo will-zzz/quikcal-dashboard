3b576aca6399193d698c2ca72da76d4d16ba0329
Will Zakielarz <wmz1st@gmail.com>
2024-04-25 18:34:07 -0400

    Day stats mockup
    Added second half to daily component. Has placeholders for number of deliveries etc.

--- src/components/DayInfo.js
+++ src/components/DayInfo.js
@@ -19,26 +19,33 @@ const DayInfo = ({ response, day }) => {
   }, [day]);
 
   return (
-    <div className="flex flex-col justify-start w-3/4 h-[35vh] overflow-auto bg-white rounded-2xl shadow-lg mt-4 px-2">
-      <h2 className="text-3xl mt-1 text-center">{weekDay}</h2>
-      {/* Map company cards whose date is current day */}
-      {response &&
-        response.map((delivery) => {
-          const deliveryDate = new Date(delivery.date);
+    <div className="flex flex-row w-3/4 h-[35vh] overflow-auto bg-white mt-4 px-2 rounded-2xl shadow-lg pt-2">
+      <div className="flex flex-col justify-start w-2/3 overflow-auto mr-2">
+        <h2 className="text-3xl text-center">{weekDay}</h2>
+        {/* Map company cards whose date is current day */}
+        {response &&
+          response.map((delivery) => {
+            const deliveryDate = new Date(delivery.date);
 
-          if (deliveryDate.toString() === day.toString()) {
-            return (
-              <CompanyCard
-                key={delivery._id}
-                name={delivery.name}
-                start={delivery.start}
-                end={delivery.end}
-                info={delivery.description}
-              />
-            );
-          }
-          return null;
-        })}
+            if (deliveryDate.toString() === day.toString()) {
+              return (
+                <CompanyCard
+                  key={delivery._id}
+                  name={delivery.name}
+                  start={delivery.start}
+                  end={delivery.end}
+                  info={delivery.description}
+                />
+              );
+            }
+            return null;
+          })}
+      </div>
+      <div className="flex flex-col w-1/3 bg-gray-200 mb-2 rounded-xl text-center justify-center space-y-10 text-2xl">
+        <p>Deliveries: 4</p>
+        <p>Delivered: 0</p>
+        <p>To-be Delivered: 4</p>
+      </div>
     </div>
   );
 };
